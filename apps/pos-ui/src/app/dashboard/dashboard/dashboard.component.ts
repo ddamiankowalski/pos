16bd42317b7418d550a6e74698bbc695071cebee
Damian <dkowalski.1997@gmail.com>
2025-09-26 17:32:44 +0200

    wip

--- apps/pos-ui/src/app/dashboard/dashboard/dashboard.component.ts
+++ apps/pos-ui/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'pos-dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrl: 'dashboard.component.scss',
-  imports: [ButtonModule],
+  imports: [ButtonModule, AvatarModule],
 })
 export class DashboardComponent {
   public items: MenuItem[] = [
