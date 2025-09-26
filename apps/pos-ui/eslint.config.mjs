91ceb036cc3a413ec5b990f33b0c769095021db0
Damian Kowalski <dkowalski.1997@gmail.com>
2025-09-26 16:05:37 +0200

    added dashboard

--- apps/pos-ui/eslint.config.mjs
+++ apps/pos-ui/eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'pos',
           style: 'kebab-case',
         },
       ],
--- apps/pos-ui/src/app/app.ts
+++ apps/pos-ui/src/app/app.ts
@@ -1,37 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pos-root',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <div class="bg-surface-50 dark:bg-surface-950 p-20">
-      <div
-        class="bg-surface-0 dark:bg-surface-900 p-6 shadow rounded-2xl flex flex-col gap-4"
-      >
-        <div class="flex gap-4">
-          <div class="flex flex-col gap-2 flex-1">
-            <div
-              class="text-2xl leading-tight font-semibold text-surface-900 dark:text-surface-0"
-            >
-              Card Title
-            </div>
-            <div
-              class="text-base leading-tight text-surface-500 dark:text-surface-300"
-            >
-              Vivamus id nisl interdum, blandit augue sit amet, eleifend mi.
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-1">
-          <div
-            class="flex-1 border-2 border-dashed border-surface-200 dark:border-surface-700 rounded-lg h-[150px]"
-          ></div>
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: 'app.html',
+  imports: [DashboardComponent],
 })
 export class App {
   protected title = 'pos-ui';
