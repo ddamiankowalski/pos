faf744b17adbd88347a767553d68362b45cef498
Damian Kowalski <dkowalski.1997@gmail.com>
2025-09-26 16:40:03 +0200

    color fix

--- apps/pos-ui/tailwind.config.js
+++ apps/pos-ui/tailwind.config.js
@@ -1,5 +1,5 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('path'); 
+const { join } = require('path');
 
 import PrimeUI from 'tailwindcss-primeui';
 
@@ -10,7 +10,36 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "var(--primary-color)",
+          50: "var(--primary-50)",
+          100: "var(--primary-100)",
+          200: "var(--primary-200)",
+          300: "var(--primary-300)",
+          400: "var(--primary-400)",
+          500: "var(--primary-500)",
+          600: "var(--primary-600)",
+          700: "var(--primary-700)",
+          800: "var(--primary-800)",
+          900: "var(--primary-900)",
+        },
+        surface: {
+          0: "var(--surface-0)",
+          50: "var(--surface-50)",
+          100: "var(--surface-100)",
+          200: "var(--surface-200)",
+          300: "var(--surface-300)",
+          400: "var(--surface-400)",
+          500: "var(--surface-500)",
+          600: "var(--surface-600)",
+          700: "var(--surface-700)",
+          800: "var(--surface-800)",
+          900: "var(--surface-900)",
+        },
+      },
+    },
   },
   plugins: [PrimeUI],
 };
