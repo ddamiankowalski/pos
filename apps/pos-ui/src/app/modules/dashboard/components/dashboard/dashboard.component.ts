6bb19a4f42ad9916b510bb3995e1189a7f3d1692
Damian <dkowalski.1997@gmail.com>
2025-09-30 22:25:53 +0200

    removed dead code

--- apps/pos-ui/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ apps/pos-ui/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { AvatarModule } from 'primeng/avatar';
-import { FormsModule } from '@angular/forms';
-import { SplitterModule } from 'primeng/splitter';
-import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabelModule } from 'primeng/floatlabel';
+import { Splitter } from 'primeng/splitter';
+import { InputText } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
 import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'pos-dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrl: 'dashboard.component.scss',
-  imports: [
-    ButtonModule,
-    AvatarModule,
-    FormsModule,
-    SplitterModule,
-    InputTextModule,
-    FloatLabelModule,
-    MenuComponent,
-  ],
+  imports: [Splitter, InputText, FloatLabel, MenuComponent],
 })
 export class DashboardComponent {}
