586bd22980dc7f1a00230075320c6139c7e3da1f
Damian <dkowalski.1997@gmail.com>
2025-10-01 19:03:56 +0200

    extracted dashboard header component

---
+++ apps/pos-ui/src/app/modules/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-dashboard-header',
+  templateUrl: 'dashboard-header.component.html',
+  imports: [],
+})
+export class DashboardHeaderComponent {}
--- apps/pos-ui/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ apps/pos-ui/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -3,11 +3,18 @@ import { Splitter } from 'primeng/splitter';
 import { InputText } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { MenuComponent } from '../menu/menu.component';
+import { DashboardHeaderComponent } from '../dashboard-header/dashboard-header.component';
 
 @Component({
   selector: 'pos-dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrl: 'dashboard.component.scss',
-  imports: [Splitter, InputText, FloatLabel, MenuComponent],
+  imports: [
+    Splitter,
+    InputText,
+    FloatLabel,
+    MenuComponent,
+    DashboardHeaderComponent,
+  ],
 })
 export class DashboardComponent {}
