e48de98dff25672825a1ea5d03180d5421c56b93
Damian <dkowalski.1997@gmail.com>
2025-10-11 13:08:03 +0200

    added routing for residents and finances

--- apps/pos-ui/src/app/app.routes.ts
+++ apps/pos-ui/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Route } from '@angular/router';
 import { ResidentsComponent } from './modules/residents/components/residents/residents.component';
+import { FinancesComponent } from './modules/finances/components/finances/finances.component';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
+    path: 'residents',
     component: ResidentsComponent,
   },
+  {
+    path: 'finances',
+    component: FinancesComponent,
+  },
+  { path: '**', redirectTo: 'residents' },
 ];
---
+++ apps/pos-ui/src/app/modules/finances/components/finances/finances.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-finances',
+  template: ` I am finances `,
+  imports: [],
+})
+export class FinancesComponent {}
