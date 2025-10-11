d2b5e37b3c42674785dfd7f79ee49a488b8ed5b2
Damian Kowalski <dkowalski.1997@gmail.com>
2025-10-11 16:37:52 +0200

    login page

--- apps/pos-ui/src/app/modules/user/components/login/login.component.ts
+++ apps/pos-ui/src/app/modules/user/components/login/login.component.ts
@@ -2,12 +2,20 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'pos-login',
   templateUrl: 'login.component.html',
-  imports: [FormsModule, PasswordModule, ButtonModule],
+  imports: [
+    FormsModule,
+    PasswordModule,
+    ButtonModule,
+    InputTextModule,
+    CheckboxModule,
+  ],
 })
 export class LoginComponent {
   username = '';
