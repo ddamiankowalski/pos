f7bd483e05a49beee2b980567451476305981352
Damian Kowalski <dkowalski.1997@gmail.com>
2025-12-07 12:20:05 +0100

    fixes

--- apps/pos-ui/src/app/modules/clients/components/clients-create/clients-create.component.ts
+++ apps/pos-ui/src/app/modules/clients/components/clients-create/clients-create.component.ts
@@ -1,13 +1,29 @@
 import { Component, model } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ButtonGroupModule } from 'primeng/buttongroup';
 import { DialogModule } from 'primeng/dialog';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'pos-clients-create',
   templateUrl: 'clients-create.component.html',
-  imports: [DialogModule, FloatLabelModule, SelectModule, ButtonModule],
+  imports: [
+    DialogModule,
+    FloatLabelModule,
+    SelectModule,
+    ButtonModule,
+    ButtonModule,
+    ButtonGroupModule,
+    InputTextModule,
+    FloatLabelModule,
+    SelectModule,
+    InputIconModule,
+    IconFieldModule,
+  ],
 })
 export class ClientsCreateComponent {
   public visible = model(false);
